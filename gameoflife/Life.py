e54fdb96a3452de8367d3186a52d6ae33e814593
ChrisWilliamson123 <cjwilliamson109@gmail.com>
2017-02-19 22:46:42 +0000

    Added a test and functionality to kill a cell if it has less than two neighbours.

--- gameoflife/Life.py
+++ gameoflife/Life.py
@@ -4,5 +4,26 @@ class Life(object):
     def __init__(self, state):
         self.state = state
 
+    def neighbours(self, coords):
+        """
+        Returns a set containing the coordinates of the specified cells neighbours
+        """
+        neighbours_set = set()
+        x = coords[0]
+        y = coords[1]
+        for i in range(x-1, x+2):
+            for j in range(y-1, y+2):
+                if (i, j) != (x, y):
+                    neighbours_set.add((i, j))
+        return neighbours_set
+
     def evolve(self):
-        self.state = set()
+        next_state = set()
+        for cell in self.state:
+            neighbours = self.neighbours(cell)
+            # Get the intersection of the current state and the neighbours
+            # This gives us the coords of the cells alive neighbours
+            alive_neighbours = set(neighbours & self.state)
+            if len(alive_neighbours) >= 2:
+                next_state.add(cell)
+        self.state = next_state
--- tests/testlife.py
+++ tests/testlife.py
@@ -2,12 +2,42 @@ from gameoflife.Life import Life
 import unittest
 
 class testLife(unittest.TestCase):
+
+    def grid_to_coords(self, grid):
+        """
+        Converts a 2D list to a set of coordinates.
+        """
+        coords = set()
+        for i in range(0, len(grid)):
+            for j in range(0, len(grid[i])):
+              if grid[i][j]:
+                  coords.add((i, j))
+
+        return coords
+
     def test_no_interaction(self):
         emptyState = [
             [0,0,0],
             [0,0,0],
             [0,0,0]
         ]
-        testLife = Life(emptyState)
+        testLife = Life(self.grid_to_coords(emptyState))
         testLife.evolve()
         self.assertEqual(set(), testLife.state)
+
+    def test_cell_death(self):
+        """
+        Tests that a cell dies if it has less than two neighbours.
+        """
+        starting_state = [
+            [1, 0, 1],
+            [0, 1, 1],
+            [0, 0, 0]
+        ]
+        testLife = Life(self.grid_to_coords(starting_state))
+        testLife.evolve()
+        self.assertEqual(self.grid_to_coords([
+            [0, 0, 1],
+            [0, 1, 1],
+            [0, 0, 0]
+        ]), testLife.state)
